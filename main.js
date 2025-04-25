f11a9ddc9d331f37b0edb68640507ef913092d45
Marco Lee <ylee0147@student.monash.edu>
2025-04-26 01:57:17 +1000

    notes saved in json file

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const fs = require('fs');
 
-function createWindow () {
+function createWindow() {
     const win = new BrowserWindow({
         width: 1000,
         height: 700,
@@ -12,5 +13,21 @@ function createWindow () {
 
     win.loadFile('index.html');
 }
+
+const NOTES_PATH = path.join(__dirname, 'notes.json');
+
+// Handle save from renderer
+ipcMain.on('save-note', (event, content) => {
+    fs.writeFileSync(NOTES_PATH, JSON.stringify({ note: content }, null, 2));
+});
+
+// Handle load to renderer
+ipcMain.handle('load-note', () => {
+    if (fs.existsSync(NOTES_PATH)) {
+        const data = fs.readFileSync(NOTES_PATH);
+        return JSON.parse(data).note || '';
+    }
+    return '';
+});
 
 app.whenReady().then(createWindow);
--- preload.js
+++ preload.js
@@ -2,3 +2,10 @@
 window.addEventListener('DOMContentLoaded', () => {
     // You can safely expose APIs later here
 });
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('notesAPI', {
+    saveNote: (content) => ipcRenderer.send('save-note', content),
+    loadNote: () => ipcRenderer.invoke('load-note'),
+});
